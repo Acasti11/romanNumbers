9afbf715a7c2fb508447acaee26bd73df1cee4c9
Andoni <110305acasti@gmail.com>
2026-04-21 09:30:17 +0200

    test:_test_2_devuelve_II

--- modulos/romanNumber.test.ts
+++ modulos/romanNumber.test.ts
@@ -4,4 +4,8 @@ describe('function convertToRomanNumber',()=>{
     test('cuando number sea 1 devuelve I',()=>{
         expect(convertToRomanNumber(1)).toBe('I');
     });
+
+    test('cuando number sea 2 devuelve II',()=>{
+        expect(convertToRomanNumber(2)).toBe('II');
+    });
 });
